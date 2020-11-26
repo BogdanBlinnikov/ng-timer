b8fb07f793b6cdf22dae4b6254e41466682e1bec
BogdanBlinnikov <bodya4ka@gmail.com>
2020-11-26 10:24:16 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-timer';
+  miliSeconds = 0;
+  seconds = 0;
+  minutes = 0;
+  intervalMs;
+  isSingleClick = true;
+  // tslint:disable-next-line:typedef
+  startTimer() {
+    this.isSingleClick = true;
+    setTimeout(() => {
+      if (this.isSingleClick){
+        this.intervalMs = setInterval(() => {
+          this.miliSeconds++;
+          if (this.miliSeconds === 100) {
+            this.miliSeconds = 0;
+            this.seconds++;
+            if (this.seconds === 60) {
+              this.seconds = 0;
+              this.minutes++;
+              if (this.minutes === 60) {
+                this.minutes = 0;
+              }
+            }
+          }
+        }, 10);
+      }
+    }, 300);
+  }
+  // tslint:disable-next-line:typedef
+  pauseTimer() {
+    this.isSingleClick = false;
+    clearInterval(this.intervalMs);
+  }
+  // tslint:disable-next-line:typedef
+  stopTimer(){
+    clearInterval(this.intervalMs);
+    this.miliSeconds = 0;
+    this.seconds = 0;
+    this.minutes = 0;
+  }
+  // tslint:disable-next-line:typedef
+  resetTimer(){
+    this.miliSeconds = 0;
+    this.seconds = 0;
+    this.minutes = 0;
+  }
 }
